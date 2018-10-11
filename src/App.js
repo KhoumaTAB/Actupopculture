f9b1750d8a2f8862ccef1abb9bdcb1899143f5e3
Michaël BOURMEAU <michael.bourmeau@gmail.com>
2018-10-11 14:08:03 +0200

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Container } from "reactstrap";
+
 import Header from "./Header";
+import NavbarSection from "./Navbar";
 
 class App extends Component {
   render() {
@@ -9,6 +11,7 @@ class App extends Component {
       <div className="App">
         <Container fluid>
           <Header />
+          <NavbarSection />
         </Container>
       </div>
     );
